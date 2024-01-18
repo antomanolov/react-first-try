aea338711715f8f41919b44ff7110b01da252a2e
Anton Manolov <mramanolov@gmail.com>
2024-01-18 09:05:09 +0200

    Tryed to implement the Avoid duplication in state rule

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -84,4 +84,60 @@ function Person() {
     );
   }
 
-export {App, Person, CustomInput}
+  
+
+  const initialItems = [
+    { title: 'pretzels', id: 0 },
+    { title: 'crispy seaweed', id: 1 },
+    { title: 'granola bar', id: 2 },
+  ];
+  
+  export default function Menu() {
+    const [items, setItems] = useState(initialItems);
+    const [selectedId, setSelectedId] = useState(0);
+  
+    const selectedItem = items.find(item =>
+      item.id === selectedId
+    );
+
+    // this checks all items if the current item id is found the title is changed
+    // BUT it's made NEW OBJECT! the object should not be MUTATED!
+    function handleItemChange(id, e) {
+      setItems(items.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            title: e.target.value,
+          };
+        } else {
+          return item;
+        }
+      }));
+    }
+  
+    return (
+      <>
+        <h2>What&apos;s your travel snack?</h2>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <input
+                value={item.title}
+                onChange={e => {
+                  handleItemChange(item.id, e)
+                }}
+              />
+              {' '}
+              <button onClick={() => {
+                setSelectedId(item.id);
+              }}>Choose</button>
+            </li>
+          ))}
+        </ul>
+        <p>You picked {selectedItem.title}.</p>
+      </>
+    );
+  }
+  
+
+export {App, Person, CustomInput, Menu}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import "./index.css";
-import {App, Person, CustomInput} from "./Greetings";
+import {App, Person, CustomInput, Menu} from "./Greetings";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,5 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App/>
         <Person/>
         <CustomInput/>
+        <Menu/>
     </React.StrictMode>
 );
